acb3e5bb0a49d2cf8aa4d4f07879de3c5cd1d367
Ahnyeongjun <toinin@naver.com>
2025-07-02 23:41:29 +0900

    .

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import webpack from "webpack";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -13,7 +12,12 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
-  staticDirs: ["../public"], // storybook에서 public 폴더를 사용하기 위해 추가
+  staticDirs: [
+    {
+      from: "../public",
+      to: process.env.GITHUB_PAGES === "true" ? "/SWYP_FRONT" : "/"
+    }
+  ],
 
   webpackFinal: async (config) => {
     // framer-motion과 관련된 모듈 해결 문제 수정
@@ -24,21 +28,6 @@ const config: StorybookConfig = {
         '@emotion/styled': require.resolve('@emotion/styled'),
       };
     }
-
-    config.plugins = config.plugins || [];
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        "process.env.GITHUB_PAGES": JSON.stringify(process.env.GITHUB_PAGES || "false"),
-      })
-    );
-
-    if (config.output) {
-      config.output.publicPath = (process.env.GITHUB_PAGES === "true") ? "/SWYP_FRONT/" : "/";
-    }
-
-
     return config;
   },
 };
-
-export default config;
